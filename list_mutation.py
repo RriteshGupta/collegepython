0db8f64f2924e13833ae7463f8d948a5a68eb20a
Ritesh Gupta <riteshkumargupta4578@gmail.com>
2026-09-21 15:21:54 +0530

    Update list_mutation.py

--- list_mutation.py
+++ list_mutation.py
@@ -1,14 +1,13 @@
-def remove_list(list):
+def remove_list(list):#pop the number in the list 6
     list.pop()
     return 
 
 list = [1,2,3,4,5,6]
-remove_list(list)
+remove_list(list)#call the functin rmove
 
 
 for i in list:
-    print(i)
-
+    print(i)#list = [1,2,3,4,5]
 
      
 
