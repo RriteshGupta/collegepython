c090abc6d55dda8fdc3f923b0d6f247821f310c7
Ritesh Gupta <riteshkumargupta4578@gmail.com>
2026-09-21 15:16:58 +0530

    Refactor temperature conversion function

--- convert_temprature.py
+++ convert_temprature.py
@@ -1,9 +1,9 @@
-n = int(input("Enter the celsius temprature"))
+n = int(input("Enter the celsius temprature")) # input = 37 in celsius
 
 
 def convert(n):
     f = (n*9/5)+32
 
-    print("The Fahrenheit temprature = ",f)
+    print("The Fahrenheit temprature = ",f) # out put 100 fahrenheit
 
 convert(n)
