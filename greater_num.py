2cca64a4849b91e173b42045d83d2d984bb7ea03
Ritesh Gupta <riteshkumargupta4578@gmail.com>
2026-09-21 15:18:27 +0530

    Fix input prompts and comments in greater_num.py

--- greater_num.py
+++ greater_num.py
@@ -1,9 +1,9 @@
-n1 = int(input("Enter the first number = "))
-n2 = int(input("Enter the second number = "))
+n1 = int(input("Enter the first number = ")) #input 23
+n2 = int(input("Enter the second number = ")) #input 22
 
 def greter_num(n1,n2):
     if n1>n2:
-        print("n1 is greter number",n1)
+        print("n1 is greter number",n1) #output 23
     else:
         print("n2 is gretaer number",n2)
 
